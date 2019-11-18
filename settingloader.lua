315de5fa452b48354337dc5156979bee5e6d4cef
ryu-raptor <ryu_omnibook@icloud.com>
2019-11-18 12:40:27 +0900

    Add settingloader.lua
    This module helps to load `setting.lua`.
    Supports inheritance of setting which can load other settings in a setting file.

---
+++ settingloader.lua
@@ -0,0 +1,39 @@
+-- 設定読み込み
+
+local function dirname(str)
+    -- スラッシュチェック
+    if not str:match("/") then
+        return "./"
+    end
+    return str:match("^(.+/)")
+end
+
+function loadSetting(settingFilePath) --> table
+    local tmp = dofile(settingFilePath)
+
+    if tmp.inherits ~= nil then
+        local root = loadSetting(tmp.inherits)
+        -- merge setting
+        -- 継承ファイルのディレクトリを取得
+        local inheritsdir = dirname(tmp.inherits)
+        -- root のテンプレートパスを書き換える
+        for k, v in pairs(root) do
+            -- テンプレートパス
+            local kk = k:match("_template$")
+            if kk then
+                local origin = root[k]
+                -- テンプレートパスを書き換え
+                root[k] = inheritsdir .. origin
+            end
+        end
+
+        -- ローカルを上書き
+        for k, v in pairs(tmp) do
+            root[k] = v
+        end
+        tmp = root
+    end
+
+    return tmp
+end
+
